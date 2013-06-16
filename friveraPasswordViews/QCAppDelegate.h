4514713b2e51871223067f677999fe3cf8e3b26b
frankleo <thisisfrankrivera@gmail.com>
2013-06-16 14:21:33 -0400

    Initial Commit

---
+++ friveraPasswordViews/QCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  QCAppDelegate.h
+//  friveraPasswordViews
+//
+//  Created by Frank Rivera on 6/16/13.
+//  Copyright (c) 2013 Frank Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class QCViewController;
+
+@interface QCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) QCViewController *viewController;
+
+@end
---
+++ friveraPasswordViews/QCViewController.h
@@ -0,0 +1,13 @@
+//
+//  QCViewController.h
+//  friveraPasswordViews
+//
+//  Created by Frank Rivera on 6/16/13.
+//  Copyright (c) 2013 Frank Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCViewController : UIViewController
+
+@end
